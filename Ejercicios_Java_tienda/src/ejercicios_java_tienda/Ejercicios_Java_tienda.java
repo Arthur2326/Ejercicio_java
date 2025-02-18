c0c9dfc3715f02d9fac3814990e960b47278b19e
Arthur2326 <penarandadayron@gmail.com>
2025-02-17 20:16:28 -0500

    Add files via upload

---
+++ Ejercicios_Java_tienda/src/ejercicios_java_tienda/Ejercicios_Java_tienda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios_java_tienda;
+
+/**
+ *
+ * @author SENA
+ */
+public class Ejercicios_Java_tienda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
